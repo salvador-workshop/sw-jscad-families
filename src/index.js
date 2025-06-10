ef79b5605964371f2ac49cc49d9822236139c055
RJ Salvador <salvador.workshop@gmail.com>
2025-06-10 18:54:19 -0400

    Split off from main package

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-const coreModule = require('./core');
-const utilsModule = require('./utils');
-const detailsModule = require('./details');
+"use strict"
+
 const familiesModule = require('./families');
-const buildersModule = require('./builders');
 
-const init = ({ lib }) => {
+const init = ({ lib, swLib }) => {
     const swJscad = {
-        core: coreModule.init({ lib }),
+        families: familiesModule.init({ lib, swLib }),
     }
-
-    swJscad.utils = utilsModule.init({ lib, swLib: swJscad });
-    swJscad.details = detailsModule.init({ lib, swLib: swJscad });
-    swJscad.families = familiesModule.init({ lib, swLib: swJscad });
-    swJscad.builders = buildersModule.init({ lib, swLib: swJscad });
 
     return swJscad;
 }
