bde696cd4a4fc0754af5224d343b43d056debaa3
RJ Salvador <salvador.workshop@gmail.com>
2025-07-09 11:48:35 -0400

    WIP - started next phase of standard models

---
+++ src/crafts/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/**
+ * ...
+ * @namespace families.crafts
+ */
+
+const init = ({ lib, swLib }) => {
+    const crafts = {
+        ting: 0
+    }
+
+    return crafts;
+}
+
+module.exports = { init };
---
+++ src/paper/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/**
+ * ...
+ * @namespace families.paper
+ */
+
+const init = ({ lib, swLib }) => {
+    const paper = {
+        ting: 0
+    }
+
+    return paper;
+}
+
+module.exports = { init };
